2123a67d655d0e24858fbc00b7c63049e91ed952
Punnapak <pupunbolt@gmail.com>
2023-11-10 14:50:27 +0700

    fix:tailwind configuration

--- my-app/src/component/Navbar.js
+++ my-app/src/component/Navbar.js
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import logo from '../assets/logo.svg';
 export default function Navbar() {
   return (
-    <div>
+    <div className='lg'>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
+        <div className='flex flex-row items-center mx-10 p-4'>
+            <img src={logo} alt="logo" className="w-16" />
+            <div className='flex flex-row items-center mx-10 p-4'>
+                <a className='font-epilogue font-bold'>
+                    Features
+                </a>
+                
+            </div>
+        </div>
+       
         </nav>
+
     </div>
   )
 }
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '375px', // Mobile
+        'lg': '1440px', // Desktop
+      },
+      colors: {
+        white: 'hsl(0, 0%, 98%)', // Almost White
+        gray: 'hsl(0, 0%, 41%)', // Medium Gray
+        black: 'hsl(0, 0%, 8%)', // Almost Black
+      },
+      fontSize: {
+        'body': '18px', // Font size for body copy
+      },
+      fontFamily: {
+        'epilogue': ['Epilogue', 'sans-serif'],
+      },
+      fontWeight: {
+        'normal': 500, // Epilogue 500
+        'bold': 700, // Epilogue 700
+      },
+    },
   },
   plugins: [],
 }
-
