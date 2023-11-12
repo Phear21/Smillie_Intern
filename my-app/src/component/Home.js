a907e816f5fd8c331a4fefa7874449b78aa6aaf3
Punnapak <pupunbolt@gmail.com>
2023-11-12 13:24:18 +0700

    fix:finalize

--- my-app/src/component/Home.js
+++ my-app/src/component/Home.js
@@ -50,12 +50,13 @@ export default function Home() {
             </div>
             <div className='flex flex-col h-[50vh] -mt-4 justify-center items-center'>
               
-                <h1 className='flex justify-center items-center font-bold text-[40px]  h-[20%] '>
-                    Make remote work
-                  </h1>
-                  <p className=' h-[20%]  mb-4  px-3 font-normal text-[18px] lg:text-[18px] text-gray'>
-                    Get your team in sync, no matter your location.
-                    Streamline processes, create team rituals, and watch productivity soar.
+            <h1 className='flex justify-center items-center font-bold text-[40px] m:text-[50px] h-[20%]'>
+                Make remote work
+              </h1>
+
+                  <p className=' h-[20%]  mb-4 m:h-[15%] m:mb-0 px-3 font-normal text-[18px] lg:text-[18px] text-gray'>
+                    Get your team in sync, no matter your location. <br className='md:hidden'/>
+                    Streamline processes, create team rituals, <br className='md:hidden'/>and watch productivity soar.
                   </p>
                   <div className='flex  my-4  items-center justify-center'>
                     <button className=' rounded-[18px] bg-black h-[60px] w-[160px] justify-center text-white hover:bg-white hover:text-black hover:outline'>
@@ -66,12 +67,13 @@ export default function Home() {
                   </div>
             
        
-              <div className='flex justify-center w-[100%] mt-4 h-[20%] items-center gap-[4vw] '>
-                <img className='h-4 l w-auto' src={databizlogo} alt="Databiz Logo" />
-                <img className='h-8 w-auto' src={audiophilelogo} alt="Audiophile Logo" />
-                <img className='h-4  w-auto' src={mettlogo} alt="Mett Logo" />
-                <img className='h-6 w-auto' src={makerlogo} alt="Maker Logo" />
-              </div>
+                  <div className='flex justify-center w-full md:w-[80%] mt-4 h-[20%] items-center gap-[4vw] md:gap-[8vw]'>
+                    <img className='h-4 lg:h-[2vh] max-w-[20%] w-auto' src={databizlogo} alt="Databiz Logo" />
+                    <img className='h-8 max-w-[20%] w-auto' src={audiophilelogo} alt="Audiophile Logo" />
+                    <img className='h-4 max-w-[20%] w-auto' src={mettlogo} alt="Mett Logo" />
+                    <img className='h-6 max-w-[20%] w-auto' src={makerlogo} alt="Maker Logo" />
+                  </div>
+
 
             </div>
              
